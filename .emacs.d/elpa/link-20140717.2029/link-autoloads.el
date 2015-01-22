9ad25a0d2a88ceb55b6f94e31ee54439dc0496e5
Hoothin Wang <rixixi@gmail.com>
2015-01-22 13:18:20 +0800

    ~~

--- .emacs.d/elpa/link-20140717.2029/link-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; link-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("link-pkg.el" "link.el") (21525 28062
-;;;;;;  620000 0))
-
-;;;***
-
-(provide 'link-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; link-autoloads.el ends here
